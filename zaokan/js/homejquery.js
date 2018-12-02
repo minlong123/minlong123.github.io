61229bf9edbf5a0f5dd7a2839f47e11119c1e6a9
minlong123 <44011769+minlong123@users.noreply.github.com>
2018-12-02 16:50:24 +0800

    update video
    update video

--- zaokan/js/homejquery.js
+++ zaokan/js/homejquery.js
@@ -5,12 +5,14 @@
       $('.home-dial').data("names","bb");
       $('.home-dial').css("transform","translateX(0)");
       $('.home-dial').css("WebkitTransform","translateX(0)");
+      $('.hm-vide').css("height",'0px');
     }else{
       $('.modd').css('display','none');
       $('.home-dial').css('opacity','0');
       $('.home-dial').data("names","dialgo");
       $('.home-dial').css("transform","translateX(-100%)");
       $('.home-dial').css("WebkitTransform","translateX(-100%)");
+      $('.hm-vide').css("height",'auto');
     }
   });
   $('.modd').click(function(){
@@ -20,8 +22,11 @@
       $('.home-dial').data("names","dialgo");
       $('.home-dial').css("transform","translateX(-100%)");
       $('.home-dial').css("WebkitTransform","translateX(-100%)");
+      $('.hm-vide').css("height",'auto');
     }
   })
+
+
   $('.sel-static').hover(function(){
     $('.sel-static').removeClass('selct-hm');
     $('.sel-static').find('.dial-ol').css('display','none');
